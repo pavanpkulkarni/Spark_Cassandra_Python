91baf2302d225b16e1e48d2153cf2ad778aeb50d
Pavan <kulkarnipavanp@gmail.com>
2018-04-27 13:36:48 -0400

    initial commit

---
+++ com/pavanpkulkarni/cassandra/PythonCassandra.py
@@ -0,0 +1,22 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+
+if __name__ == "__main__":
+
+    spark = SparkSession\
+        .builder \
+        .master("local[*]") \
+        .appName("understanding_sparksession") \
+        .getOrCreate()
+
+    lines = spark.read.text("/Users/pavanpkulkarni/Documents/workspace/Spark_Cassandra_Python/com/pavanpkulkarni/cassandra/sample.txt")
+
+    wordCounts = lines\
+        .select(explode(split(lines.value, "\s+"))
+        .alias("word"))\
+        .groupBy("word")\
+        .count()
+
+    print("\n Word Count : ", wordCounts.show(20, 50))
+
+    spark.stop()
